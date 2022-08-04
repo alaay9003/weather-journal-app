f8583518b87eb44edac125bbd2f085e6043500fa
alaay9003 <alaay9003@gmail.com>
2022-08-04 19:26:01 +0200

    second commit

--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'/'+ d.getDate()+'/'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
 const apiKey = "&appid=a560750b6cefeea72b5dd8e5fb01bfc1&units=imperial";
 const baseurl = "https://api.openweathermap.org/data/2.5/weather?zip=";
 const zipCode = document.getElementById('zip');
